948984c7c1d9f384b0c5de2507cee13eb92b754b
MUGALMUSHRAF123 <114667793+MUGALMUSHRAF123@users.noreply.github.com>
2024-06-14 09:47:30 +0530

    Delete type.js

--- type.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".auto-type", {
-    strings: ["Mugal Mushraf", "Web Developer", "Front End Developer", "UI/UX Designer"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-});
